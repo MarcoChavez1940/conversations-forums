23ef23e1a306646f124f44bcd67413d3102b47de
MarcoChavez1940 <marcostarr1940@gmail.com>
2018-03-10 14:12:44 -0600

    add references between the models

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180308062133) do
+ActiveRecord::Schema.define(version: 20180310194459) do
 
   create_table "chatrooms", force: :cascade do |t|
     t.string "topic"
@@ -20,8 +20,12 @@ ActiveRecord::Schema.define(version: 20180308062133) do
 
   create_table "messages", force: :cascade do |t|
     t.string "content"
+    t.integer "user_id"
+    t.integer "chatroom_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["chatroom_id"], name: "index_messages_on_chatroom_id"
+    t.index ["user_id"], name: "index_messages_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
