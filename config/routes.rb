60187c7d6cefc512ade744543bbd3e836c9074f3
William Cervera <w.cervera.p@gmail.com>
2018-03-10 15:37:53 -0600

    Adding the registrations controller, and view, without the alert message when user is taken

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   root "welcome#about"
+
+  get '/signup', to: "registrations#new"
+  post '/signup', to: "registrations#create"
   # Serve websocket cable requests in-process
   mount ActionCable.server => '/cable'
 
