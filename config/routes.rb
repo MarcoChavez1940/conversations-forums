432ce094166d1a8726ab04f1311e10a79a3422bb
MarcoChavez1940 <marcostarr1940@gmail.com>
2018-03-10 18:28:19 -0600

    add a slug to chatroom model

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,11 @@ Rails.application.routes.draw do
 
   get '/signup', to: "registrations#new"
   post '/signup', to: "registrations#create"
-  # Serve websocket cable requests in-process
-  mount ActionCable.server => '/cable'
 
   resources :chatrooms, param: :slug
   resources :messages
+
+  # Serve websocket cable requests in-process
+  mount ActionCable.server => '/cable'
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180310194459) do
+ActiveRecord::Schema.define(version: 20180311001142) do
 
   create_table "chatrooms", force: :cascade do |t|
     t.string "topic"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "slug"
   end
 
   create_table "messages", force: :cascade do |t|
