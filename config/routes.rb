4328652c1494b81826198f401268b09796fe20fa
MarcoChavez1940 <marcostarr1940@gmail.com>
2018-03-11 10:41:46 -0600

    add sessions and Login page

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,11 @@ Rails.application.routes.draw do
 
   get '/signup', to: "registrations#new"
   post '/signup', to: "registrations#create"
+
+  get 'login', to: "sessions#new"
+  post '/login', to: "sessions#create" 
+  delete '/logout', to: "sessions#destroy"
+
   # Serve websocket cable requests in-process
   mount ActionCable.server => '/cable'
 
