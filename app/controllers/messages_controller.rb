08b3983257100a51e6618adebf37722576bf516e
MarcoChavez1940 <marcostarr1940@gmail.com>
2018-03-11 12:26:12 -0600

    done the necesary to do a real time chat

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -5,8 +5,8 @@ class MessagesController < ApplicationController
         message.user = current_user
         if message.save
             ActionCable.server.broadcast 'messages',
-            message: message.content,
-            user: message.user.username
+                message: message.content,
+                user: message.user.username
             head :ok
         else 
             redirect_to chatrooms_path
