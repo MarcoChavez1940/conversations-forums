bf3048450b23c9d2c9fe3485a0e4ab149215f9db
MarcoChavez1940 <marcostarr1940@gmail.com>
2018-03-18 17:04:49 -0600

    clean input message after send a message

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,13 +3,16 @@ class MessagesController < ApplicationController
     def create
         message = Message.new(message_params)
         message.user = current_user
-        ChatroomUser.where(id: 11).destroy_all
         if message.save
             #ActionCable.server.broadcast 'messages',
             ActionCable.server.broadcast "messages_#{message.chatroom_id}_channel",
                 message: message.content,
                 user: message.user.username
-            head :ok
+
+            respond_to do |format|
+                format.html { redirect_to @message }
+                format.js
+            end   
         else 
             redirect_to chatrooms_path
         end
